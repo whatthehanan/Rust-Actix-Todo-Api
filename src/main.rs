6e11c587bfa0851c8848cd792d8e8c9ddd9d3456
Hanan <hanan.hamza@ffwagency.com>
2023-03-04 20:11:05 +0100

    feat: add carog watch task

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, main, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -14,11 +14,42 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-#[actix_web::main]
+#[get("/hanan")]
+async fn hello_hanan() -> impl Responder {
+    HttpResponse::Ok().body("hello hamza")
+}
+
+async fn html_response(data: web::Data<AppState>) -> impl Responder {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {app_name}!") // <- response with app_name
+}
+
+struct AppState {
+    app_name: String,
+}
+
+fn scoped_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/test")
+            .route(web::get().to(|| async { HttpResponse::Ok().body("test") }))
+            .route(web::head().to(HttpResponse::MethodNotAllowed)),
+    );
+}
+
+#[main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Test api 2"),
+            }))
+            .configure(scoped_config)
             .service(hello)
+            .service(
+                web::scope("/api")
+                    .service(hello_hanan)
+                    .route("/index.html", web::get().to(html_response)),
+            )
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
