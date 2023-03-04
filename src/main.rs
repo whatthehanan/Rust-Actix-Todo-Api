21ebf0315469a075124e4cbcd85d2082c2c72889
Hanan <hanan.hamza@ffwagency.com>
2023-03-04 20:18:53 +0100

    fix: remove all routes except index route

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,13 @@
-use actix_web::{get, main, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, main, web, App, HttpServer, Responder};
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-#[get("/hanan")]
-async fn hello_hanan() -> impl Responder {
-    HttpResponse::Ok().body("hello hamza")
-}
-
-async fn html_response(data: web::Data<AppState>) -> impl Responder {
-    let app_name = &data.app_name; // <- get app_name
-    format!("Hello {app_name}!") // <- response with app_name
+async fn hello(data: web::Data<AppState>) -> impl Responder {
+    let app_name = &data.app_name;
+    format!("{}", app_name)
 }
 
 struct AppState {
     app_name: String,
-}
-
-fn scoped_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/test")
-            .route(web::get().to(|| async { HttpResponse::Ok().body("test") }))
-            .route(web::head().to(HttpResponse::MethodNotAllowed)),
-    );
 }
 
 #[main]
@@ -41,17 +15,9 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .app_data(web::Data::new(AppState {
-                app_name: String::from("Test api 2"),
+                app_name: String::from("Rust Todo API"),
             }))
-            .configure(scoped_config)
             .service(hello)
-            .service(
-                web::scope("/api")
-                    .service(hello_hanan)
-                    .route("/index.html", web::get().to(html_response)),
-            )
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
